c627adb3486fdf585b31e2a7390816032c7f6bc7
skywalkerwhack“ <harmfulski@gmail.com>
2025-06-20 17:13:08 +0800

    init

---
+++ springboot/src/main/java/com/example/springboot/service/impl/QuitRoomServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.springboot.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.springboot.entity.QuitRoom;
+import com.example.springboot.mapper.QuitRoomMapper;
+import com.example.springboot.service.QuitRoomService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class QuitRoomServiceImpl extends ServiceImpl<QuitRoomMapper, QuitRoom> implements QuitRoomService {
+
+    @Resource
+    private QuitRoomMapper quitRoomMapper;
+
+    @Override
+    public int addQuit(QuitRoom quitRoom) {
+        return quitRoomMapper.insert(quitRoom);
+    }
+
+    @Override
+    public Page find(Integer pageNum, Integer pageSize, String search) {
+        Page<QuitRoom> page = new Page<>(pageNum, pageSize);
+        QueryWrapper<QuitRoom> qw = new QueryWrapper<>();
+        qw.like("username", search);
+        return quitRoomMapper.selectPage(page, qw);
+    }
+
+    @Override
+    public int deleteQuit(Integer id) {
+        return quitRoomMapper.deleteById(id);
+    }
+
+    @Override
+    public int updateQuit(QuitRoom quitRoom) {
+        return quitRoomMapper.updateById(quitRoom);
+    }
+}
---
+++ vue/src/layout/Layout.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-container style="position: relative; background-color: white; height: 100vh">
+    <!--    侧边功能栏-->
+    <el-aside width="200px">
+      <Aside/>
+    </el-aside>
+    <el-container>
+      <!--      头部-->
+      <el-header style="height: 50px;border-bottom: 1px solid #ccc;">
+        <Header/>
+      </el-header>
+      <el-main style="padding: 0;
+                      position: absolute;
+                      left: 200px;
+                      right: 0;
+                      top: 70px;
+                      bottom: 0;">
+        <!--        内容区域-->
+        <router-view/>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+
+const {ElMessage} = require("element-plus");
+
+export default {
+  name: "layout",
+  components: {
+    Header,
+    Aside,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
